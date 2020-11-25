63e13b993c5e7ee4e087a8c51b7ffe40d060cd80
Davide Coppola <vivaladav@gmail.com>
2020-11-25 08:27:19 -0500

    Add map size label in preview

--- src/Widgets/MapPreview.cpp
+++ src/Widgets/MapPreview.cpp
@@ -2,8 +2,12 @@
 
 #include "IsoMap.h"
 
+#include "graphic/Font.h"
+#include "graphic/FontManager.h"
 #include "graphic/Image.h"
+#include "graphic/Text.h"
 
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -13,8 +17,10 @@ namespace game
 MapPreview::MapPreview(const char * file, lib::sgui::Widget * parent)
     : lib::sgui::Widget(parent)
 {
+    using namespace lib::graphic;
+
     // -- BACKGROUND --
-    mBg = new lib::graphic::Image("data/img/map_preview_bg.png");
+    mBg = new Image("data/img/map_preview_bg.png");
     SetSize(mBg->GetWidth(), mBg->GetHeight());
 
 
@@ -30,37 +36,48 @@ MapPreview::MapPreview(const char * file, lib::sgui::Widget * parent)
 
     mIsoMap = new IsoMap(file, TILE_W);
     mIsoMap->SetTiles(tiles);
-}
 
-bool MapPreview::Load(const char * file)
-{
-    // load file
-    if(!mIsoMap->Load(file))
-        return false;
+    // -- LABEL SIZE --
+    std::ostringstream os;
+    os << mIsoMap->GetNumRows() << "x" << mIsoMap->GetNumCols() << std::ends;;
 
-    // reposition map
-    HandlePositionChanged();
+    Font * font = FontManager::Instance()->GetFont("data/fonts/OpenSans.ttf", 24);
 
-    return true;
+    mLabelSize = new Text(os.str().c_str(), font);
+    mLabelSize->SetColor(0x888888FF);
 }
 
 void MapPreview::HandlePositionChanged()
 {
+    // background
     const int x0 = GetScreenX();
     const int y0 = GetScreenY();
 
     mBg->SetPosition(x0, y0);
 
+    // map
     const int x = x0 + mBg->GetWidth() * 0.5f;
     const int y = y0 + (mBg->GetHeight() - mIsoMap->GetHeight()) * 0.5f;
 
     mIsoMap->SetOrigin(x, y);
+
+    // label
+    const int marginLabelT = 0;
+
+    const int x1 = x0 + (mBg->GetWidth() - mLabelSize->GetWidth()) * 0.5f;
+    const int y1 = y0 + mBg->GetHeight() + marginLabelT;
+
+    mLabelSize->SetPosition(x1, y1);
+
 }
 
 void MapPreview::OnRender()
 {
     mBg->Render();
+
     mIsoMap->Render();
+
+    mLabelSize->Render();
 }
 
 } // namespace game
