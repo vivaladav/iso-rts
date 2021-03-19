709911323f4560f6b297017dba072d4775814232
Davide Coppola <vivaladav@gmail.com>
2021-03-18 22:15:04 -0400

    Proper placing of elements in PanelResources

--- src/Widgets/PanelResourcesBar.cpp
+++ src/Widgets/PanelResourcesBar.cpp
@@ -17,23 +17,35 @@ PanelResourcesBar::PanelResourcesBar(Player * player)
 {
     SetBg();
 
-    const int x0 = 15;
+    // TODO change this to have dynamic slots added/removed at runtime
+    const int slotW = 150;
+    const int slots = 4;
+    const int slotsW = slotW * slots;
+    const int x0 = (GetWidth() - slotsW) * 0.5f;
+
+    int slotX = x0;
 
     auto rd = new ResourceDisplay("data/img/UI/icon_energy.png", this);
     rd->SetValue(100);
-    rd->SetPosition(x0 + 50, (GetHeight() - rd->GetHeight()) * 0.5f);
+    rd->SetPosition(slotX + (slotW - rd->GetWidth()) * 0.5f, (GetHeight() - rd->GetHeight()) * 0.5f);
+
+    slotX += slotW;
 
     rd = new ResourceDisplay("data/img/UI/icon_material1.png", this);
     rd->SetValue(325);
-    rd->SetPosition(x0 + 200, (GetHeight() - rd->GetHeight()) * 0.5f);
+    rd->SetPosition(slotX + (slotW - rd->GetWidth()) * 0.5f, (GetHeight() - rd->GetHeight()) * 0.5f);
+
+    slotX += slotW;
 
     rd = new ResourceDisplay("data/img/UI/icon_diamond.png", this);
     rd->SetValue(654);
-    rd->SetPosition(x0 + 350, (GetHeight() - rd->GetHeight()) * 0.5f);
+    rd->SetPosition(slotX + (slotW - rd->GetWidth()) * 0.5f, (GetHeight() - rd->GetHeight()) * 0.5f);
+
+    slotX += slotW;
 
     rd = new ResourceDisplay("data/img/UI/icon_blob.png", this);
     rd->SetValue(950);
-    rd->SetPosition(x0 + 500, (GetHeight() - rd->GetHeight()) * 0.5f);
+    rd->SetPosition(slotX + (slotW - rd->GetWidth()) * 0.5f, (GetHeight() - rd->GetHeight()) * 0.5f);
 }
 
 PanelResourcesBar::~PanelResourcesBar()
