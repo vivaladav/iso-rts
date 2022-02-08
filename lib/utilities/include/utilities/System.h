82d694393fa2081c30c01201bfc7749a81484ef7
Davide Coppola <vivaladav@gmail.com>
2022-02-08 15:07:17 +0100

    Remove lib directory

--- lib/utilities/include/utilities/System.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#if defined(LINUX)
-    #include "utilities/linux/System.h"
-#elif defined(WINDOWS)
-    #include "utilities/windows/System.h"
-#else
-    #error Platform not supported
-#endif
