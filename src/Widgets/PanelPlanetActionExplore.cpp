c8c24d949652db85212b6c56d75f49d97f2a2388
Davide Coppola <vivaladav@gmail.com>
2022-12-03 11:56:32 +0100

    Fix PanelPlanetActionExplore not resetting cancel button label

--- src/Widgets/PanelPlanetActionExplore.cpp
+++ src/Widgets/PanelPlanetActionExplore.cpp
@@ -112,6 +112,8 @@ void PanelPlanetActionExplore::UpdateExplorationStatus(TerritoryStatus status)
     else
     {
         mContentSuccess->SetVisible(false);
+
+        mButtonCancel->SetLabel("CANCEL");
     }
 }
 
