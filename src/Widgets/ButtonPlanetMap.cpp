ee4991538c5272fdfd4056bdd8e70bf97366b1af
Davide Coppola <vivaladav@gmail.com>
2022-12-07 09:29:06 +0100

    Update graphics of planet map buttons

--- src/Widgets/ButtonPlanetMap.cpp
+++ src/Widgets/ButtonPlanetMap.cpp
@@ -12,7 +12,7 @@ ButtonPlanetMap::ButtonPlanetMap(sgl::sgui::Widget * parent) :
     GameButton(SpriteFilePlanetMap,
         { IND_PM_BTN_NORMAL, IND_PM_BTN_DISABLED, IND_PM_BTN_OVER,
           IND_PM_BTN_PRESSED, IND_PM_BTN_NORMAL },
-        { 0xd7eaf4ff, 0x506c7cff, 0xebf4f9ff, 0xc3dfeeff, 0xd7eaf4ff },
+        { 0xd7eaf4ff, 0x335f73b2, 0xebf4f9ff, 0xc3dfeeff, 0xd7eaf4ff },
         parent)
 {
     const char * fileFont = "data/fonts/Lato-Regular.ttf";
@@ -27,7 +27,7 @@ SecondaryButtonPlanetMap::SecondaryButtonPlanetMap(sgl::sgui::Widget * parent) :
     GameButton(SpriteFilePlanetMap,
         { IND_PM_BTN2_NORMAL, IND_PM_BTN2_DISABLED, IND_PM_BTN2_OVER,
           IND_PM_BTN2_PRESSED, IND_PM_BTN2_NORMAL },
-        { 0xf4e5d7ff, 0x7c6650ff, 0xf9f2ebff, 0xeed9c3ff, 0xf4e5d7ff },
+        { 0xf4e5d7ff, 0x735333b2, 0xf9f2ebff, 0xeed9c3ff, 0xf4e5d7ff },
         parent)
 {
     const char * fileFont = "data/fonts/Lato-Regular.ttf";
